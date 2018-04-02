a1e67fdd748aae75e332463f7fae48fa0aa84be2
Adam Kim <akim456@gmail.com>
2018-04-02 13:59:07 -0400

    re wrote code to work better in scope

--- app/src/main/java/com/example/akim4/highlowgame/MainActivity.java
+++ app/src/main/java/com/example/akim4/highlowgame/MainActivity.java
@@ -10,44 +10,42 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Random rand = new Random();
-    int r = rand.nextInt(100)+1;
+    int r;
 
-    public void guessButton(View view){
+    public void guessButton(View view) {
 
         EditText number = (EditText) findViewById(R.id.number);
-        int userNumber =Integer.parseInt(number.getText().toString());
 
+        int userNumber = Integer.parseInt(number.getText().toString());
 
-        if (userNumber > r && userNumber < 101){
+        if (userNumber > r && userNumber < 101) {
+
             Toast.makeText(MainActivity.this, "Too high, try again!", Toast.LENGTH_SHORT).show();
             Log.i("Info", "Guess Button was pressed! user guessed too HIGH of " + number.getText().toString() + " but the random number is " + r);
-        }else if (userNumber < r && userNumber > 0) {
+
+        } else if (userNumber < r && userNumber > 0) {
+
             Toast.makeText(MainActivity.this, "Too low, try again!", Toast.LENGTH_SHORT).show();
             Log.i("Info", "Guess Button was pressed! user guessed too LOW of " + number.getText().toString() + " but the random number is " + r);
-        }else if (userNumber < 1 || userNumber > 100) {
+        } else if (userNumber < 1 || userNumber > 100) {
+
             Toast.makeText(MainActivity.this, "You must guess a number from 1-100!", Toast.LENGTH_LONG).show();
             Log.i("Info", "Guess Button was pressed! User did not follow instructions and exceeded parameters" + number.getText().toString() + " but the random number is " + r);
-        }else if (userNumber == r) {
 
-            Random rand = new Random();
-            int r = rand.nextInt(100)+1;
-
+        } else {
 
             Toast.makeText(MainActivity.this, "Congratulations, You guessed the correct number!", Toast.LENGTH_LONG).show();
             Log.i("Info", "Guess Button was pressed! User guessed CORRECTLY with " + number.getText().toString() + " and the random number was " + r);
 
-
-
         }
     }
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        r = rand.nextInt(100)+1;
+
     }
 }
